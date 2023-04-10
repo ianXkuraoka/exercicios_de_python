4c17c3bd8e528c070e60254b53e7d005ec91afdc
Ian <ian.xk9@gmail.com>
2023-04-10 04:29:23 -0300

    exercicios_slide_103_104

--- temp.py
+++ temp.py
@@ -2,17 +2,40 @@
 
 #exercícios slide 47
 
-frase = 'Esta é uma frase';
-type(frase);
+#frase = 'Esta é uma frase';
+#type(frase);
 
-a = 4;
-type(a)
+#a = 4;
+#type(a);
 
-x = 2.5;
-type(x);
+#x = 2.5;
+#type(x);
 
-Q = True;
-type(Q)
+#Q = True;
+#type(Q);
 
-s = 1 + 2j;
-type(s);
+#s = 1 + 2j;
+#type(s);
+
+#exercícios slide 103/104
+
+#wage = int(input('digite seu salário: '));
+
+#if wage >= 1200:
+#    print('Você deverá pagar impostos.');
+#else:
+#    print('você não deve pagar impostos.');
+
+#------------------------------------------
+
+nota1 = int(input('media 1 : '));
+nota2 = int(input('media 2 : '));
+nota3 = int(input('media 3 : '));
+
+media = (nota1 + nota2 + nota3) / 3;
+
+if media >= 7:
+    print('Parabéns, você passou.');
+else:
+    print('reprovado.');
+    
